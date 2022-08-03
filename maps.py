2ae7b7de4157e75826b5f694d2159af5f3065921
Databse567 <andy.moran@student.onlsow.school.nz>
2022-08-04 09:44:10 +1200

    maps 2

--- maps.py
+++ maps.py
@@ -19,7 +19,7 @@ map_1 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["i", ".", ".", ".", ".", ".", ".", ".", ".", ".", "Ð", ".", ".", ".", ".", "."],
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
 desc_1 = "Dessert Flank, 1940, south of Mersa Matruh"
-e_units_1 = {"133rd Armoured Division 'Littorio'": ["Light Armour", 10, "3"], "135th Armoured Cavalry Division 'Ariete": ["Light Armour", 10, "3"]}
+e_units_1 = {"133rd Armoured Division 'Littorio'": ["Light Armour", 10, "1"], "135th Armoured Cavalry Division 'Ariete": ["Light Armour", 10, "2"]}
 
 
 ###
@@ -37,8 +37,8 @@ map_2 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["i", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
 desc_2 = "Operation Comapass, 1940, Italian fortification around Maktila"
-e_units_2 ={"16th Infantry Division 'Pistoia'": ["Infantry", 4, "1"], "40th Infantry Division 'Cacciatori d’Africa'": ["Infantry", 4, "1"]
-, "44th Infantry Division 'Cremona'": ["Infantry", 4, "1"], "3rd Artillery Regiment 'Pistoia": ["Artillery", 2, "2"]}
+e_units_2 = {"16th Infantry Division 'Pistoia'": ["Infantry", 4, "1"], "40th Infantry Division 'Cacciatori d’Africa'": ["Infantry", 4, "2"]
+, "44th Infantry Division 'Cremona'": ["Infantry", 4, "3"], "3rd Artillery Regiment 'Pistoia": ["Artillery", 2, "4"]}
 
 ###
 # Level 3
@@ -54,8 +54,9 @@ map_3 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["h", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["i", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
-desc_3 = "El Aghelia offensive, 1941, El Aghelia"
-e_units_3 =[]
+desc_3 = "Operation Brevity, 1941, Sollum - Capuzzo - Bardia Area"
+e_units_3 = {"2nd Battalion, Pz. Regiment 5 (l).1": ["Light Armour", 10, "1"], "2nd Battalion, Pz. Regiment 5 (l).2": ["Light Armour", 10, "2"],
+ "2nd Battalion, Pz. Regiment 5 (m)": ["Medium Armour", 5, "3"], "Trento Division": ["Infantry", 3, "4"], "Artillery Regiment 'Trento'": ["Artillery", 1, "5"]}
 
 ###
 # Level 4
@@ -71,8 +72,9 @@ map_4 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["h", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["i", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
-desc_4 = "Operation Brevity, 1941, Sollum - Capuzzo - Bardia Area"
-e_units_4 =[]
+desc_4 = "Battle of Sidi Rezegh, 1941, South of Tobruk"
+e_units_4 = {"25th Bologna Infatry Division": ["Infantry", 3, "1"], "17th Infantry Division 'Pavia'": ["Infantry", 3, "1"], "21st Panzer": ["Medium Armour", 5, "4"],
+ "15th Panzer": ["Medium Armour", 5, "4"], "309th Artillery Division": ["Artillery", 1, "2"], "21st Panzer (l)": ["Light Armour", 10, "3"]}
 
 ###
 # Level 5
@@ -88,8 +90,9 @@ map_5 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["h", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["i", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
-desc_5 = "Operation Crusader, 1942, Coastal road between El-alamain and Tobruk"
-e_units_5 =[]
+desc_5 = "Second action at Bir El Gubi, 1942, Outside of Cyenaica"
+e_units_5 = {"Infantry Divsion (Giovani Fascisti) VB.1": ["Infantry", 3, "1"], "test 1_e": ["Infantry", 3, "1"], "test 1_e": ["Infantry", 3, "1"],
+"test 1_e": ["Infantry", 3, "1"], "test 2_e": ["Artillery", 1, "2"], "test 2_e": ["Artillery", 1, "2"], "test 4_e": ["Medium Armour", 5, "4"]}
 
 maps_dict = [map_1, map_2, map_3, map_4, map_5]
 desc_dict = [desc_1, desc_2, desc_3, desc_4, desc_5]
