885b90aa817d1440fb990e38d43bff432de0f256
Databse567 <andy.moran@student.onlsow.school.nz>
2022-08-09 14:26:57 +1200

    more map updates

--- maps.py
+++ maps.py
@@ -5,6 +5,24 @@
 # stores the maps
 
 ###
+# Test level
+###
+map_t = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"],
+["a", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["b", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["c", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["d", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["e", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["f", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["g", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["h", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["i", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
+desc_t = "Test level, 1812, in your mums closet"
+e_units_t = {"Light tank":["Light Armour", 10, "1"], "Infantry": ["Infantry", 4, "2"], "Artillery": ["Artillery", 2, "3"]}
+reward_t = 5
+
+###
 # Level 1
 ###
 map_1 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"],
@@ -20,7 +38,7 @@ map_1 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
 desc_1 = "Dessert Flank, 1940, south of Mersa Matruh"
 e_units_1 = {"133rd Armoured Division 'Littorio'": ["Light Armour", 10, "1"], "135th Armoured Cavalry Division 'Ariete": ["Light Armour", 10, "2"]}
-
+reward_1 = 5
 
 ###
 # Level 2
@@ -39,6 +57,7 @@ map_2 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 desc_2 = "Operation Comapass, 1940, Italian fortification around Maktila"
 e_units_2 = {"16th Infantry Division 'Pistoia'": ["Infantry", 4, "1"], "40th Infantry Division 'Cacciatori d’Africa'": ["Infantry", 4, "2"]
 , "44th Infantry Division 'Cremona'": ["Infantry", 4, "3"], "3rd Artillery Regiment 'Pistoia": ["Artillery", 2, "4"]}
+reward_2 = 7
 
 ###
 # Level 3
@@ -57,6 +76,7 @@ map_3 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 desc_3 = "Operation Brevity, 1941, Sollum - Capuzzo - Bardia Area"
 e_units_3 = {"2nd Battalion, Pz. Regiment 5 (l).1": ["Light Armour", 10, "1"], "2nd Battalion, Pz. Regiment 5 (l).2": ["Light Armour", 10, "2"],
  "2nd Battalion, Pz. Regiment 5 (m)": ["Medium Armour", 5, "3"], "Trento Division": ["Infantry", 3, "4"], "Artillery Regiment 'Trento'": ["Artillery", 1, "5"]}
+reward_3 = 9
 
 ###
 # Level 4
@@ -75,6 +95,7 @@ map_4 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 desc_4 = "Battle of Sidi Rezegh, 1941, South of Tobruk"
 e_units_4 = {"25th Bologna Infatry Division": ["Infantry", 3, "1"], "17th Infantry Division 'Pavia'": ["Infantry", 3, "1"], "21st Panzer": ["Medium Armour", 5, "4"],
  "15th Panzer": ["Medium Armour", 5, "4"], "309th Artillery Division": ["Artillery", 1, "2"], "21st Panzer (l)": ["Light Armour", 10, "3"]}
+reward_4 = 11
 
 ###
 # Level 5
@@ -91,9 +112,13 @@ map_5 = [[" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 ["i", ".", "Ð", ".", ".", ".", ".", ".", ".", ".", ".", ".", "Ð", "Ð", ".", "."],
 ["j", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
 desc_5 = "Second action at Bir El Gubi, 1942, Outside of Cyenaica"
-e_units_5 = {"Infantry Divsion (Giovani Fascisti) VB.1": ["Infantry", 3, "1"], "test 1_e": ["Infantry", 3, "1"], "test 1_e": ["Infantry", 3, "1"],
-"test 1_e": ["Infantry", 3, "1"], "test 2_e": ["Artillery", 1, "2"], "test 2_e": ["Artillery", 1, "2"], "test 4_e": ["Medium Armour", 5, "4"]}
+e_units_5 = {"Infantry Divsion (Giovani Fascisti) VB.1": ["Infantry", 3, "1"], "Infantry Divsion (Giovani Fascisti) VB.2": ["Infantry", 3, "2"],
+ "Infantry Divsion (Giovani Fascisti) VB.3": ["Infantry", 3, "3"], "Trieste Division": ["Infantry", 3, "4"], 
+ "Artillery regiment(Giovani Fascisti).1": ["Artillery", 1, "2"], "Artillery regiment(Giovani Fascisti).2": ["Artillery", 1, "2"], 
+ "15th Pz. Division": ["Medium Armour", 5, "4"]}
+reward_5 = 15
 
-maps_dict = [map_1, map_2, map_3, map_4, map_5]
-desc_dict = [desc_1, desc_2, desc_3, desc_4, desc_5]
-e_form_dict = [e_units_1, e_units_2, e_units_3, e_units_4, e_units_5]
+maps_dict = [map_1, map_2, map_3, map_4, map_5, map_t]
+desc_dict = [desc_1, desc_2, desc_3, desc_4, desc_5, desc_t]
+e_form_dict = [e_units_1, e_units_2, e_units_3, e_units_4, e_units_5, e_units_t]
+rewards = [reward_1, reward_2, reward_3, reward_4, reward_5, reward_t]
